459a138fe16450ff608e43fc457536510ab45fb6
s-araqayyum <saraqayyum268@gmail.com>
2024-03-27 17:04:53 +0500

    Feat: Frontend and Dockerization

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient("mongodb://mongo:27017/")
 db = client.user_db
